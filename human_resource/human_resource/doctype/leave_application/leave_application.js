00ba36edbdbdbcd486629ecc1bc01031b40a463e
mahmoodhoroun <horounmahmood@gmail.com>
2023-01-27 00:31:38 +0200

    new update

---
+++ human_resource/human_resource/doctype/leave_application/leave_application.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2023, mahmood and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Leave Application', {
+	// refresh: function(frm) {
+
+	// }
+	// validate: function(frm) {
+    //     if (frm.doc.from_date < frm.doc.to_date) {
+	// 		console.log(frm.doc.from_date.day);
+	// 		console.log("000")
+
+    //         frappe.throw("Please select a From Date from the present or future.");
+    //     }
+	// }
+});
