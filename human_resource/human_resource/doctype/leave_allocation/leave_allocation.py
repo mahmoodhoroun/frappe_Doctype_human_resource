a024f9167394e380ea4bc4301c389a3f6c68eb8c
mahmoodhoroun <horounmahmood@gmail.com>
2023-01-28 20:05:52 +0200

    new update

--- human_resource/human_resource/doctype/leave_allocation/leave_allocation.py
+++ human_resource/human_resource/doctype/leave_allocation/leave_allocation.py
@@ -9,6 +9,7 @@ from frappe.model.document import Document
 class leaveAllocation(Document):
 	def validate(self):
 		self.validate_for_leave_allocation()
+		
 
 	def validate_for_leave_allocation(self):
 		leave_allocation_in_same_time = True
@@ -16,8 +17,10 @@ class leaveAllocation(Document):
 		(self.employee, self.leave_type), as_dict=1)
 		print(leave_allocations_for_same_employee)
 		new_from_date = datetime.strptime(self.from_date, '%Y-%m-%d').date()
+		new_to_date = datetime.strptime(self.to_date, '%Y-%m-%d').date()
+
 		for x in leave_allocations_for_same_employee:
-				if x.from_date <= new_from_date <= x.to_date and self.employee == x.employee and self.leave_type == x.leave_type:
+				if x.from_date <= new_from_date <= x.to_date and x.from_date <= new_to_date <= x.to_date and self.employee == x.employee and self.leave_type == x.leave_type:
 					leave_allocation_in_same_time = False
 					print(leave_allocations_for_same_employee)
 
